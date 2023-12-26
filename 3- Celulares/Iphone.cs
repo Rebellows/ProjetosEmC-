6e9d86fc17b2ab972d88a94672efe6679e4952b1
Gabriel Dorneles Rebello <130177863+Rebellows@users.noreply.github.com>
2023-12-26 18:04:09 -0300

    Add files via upload

---
+++ 3- Celulares/Iphone.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetosEmC_.Models
+{
+    // TODO: Herdar da classe "Smartphone"
+    public class Iphone : Smartphone
+    {
+        public  Iphone(string numero, string imie, string modelo, int memoria) : base(numero, imie, modelo, memoria)
+        {
+
+        }        
+        public override void InstalarAplicativo(string nomeApp) {
+            Console.WriteLine($"Aplicativo {nomeApp} instalado com sucesso no Iphone");
+        }
+    }
+}
---
+++ 3- Celulares/Program.cs
@@ -0,0 +1,12 @@
+﻿using ProjetosEmC_.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia("123", "111111111", "Modelo X", 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("ChatGPT");
+Console.WriteLine("\n");
+Console.WriteLine("Smartphone iPhone:");
+Iphone iphone = new Iphone("456", "222222222", "Modelo Y", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("BingAI");
